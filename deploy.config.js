911d35b275f8f9156ffd997e7249a3dc26421958
andriatiana <andry.andriatiana@obs-mip.fr>
2022-09-26 11:36:41 +0200

    update cdn url

--- deploy.config.js
+++ deploy.config.js
@@ -1,6 +1,6 @@
 require('dotenv').config() // Get Env variables to webpack
 const UploadPlugin = require("sedoo-static-upload-plugin");
-const url = "https://services.aeris-data.fr/cdn/blockrepo/v1_0/upload/"
+const url = "https://api.sedoo.fr/aeris-cdn-rest/blockrepo/v1_0/upload/"
 module.exports = {
   
   plugins: [
